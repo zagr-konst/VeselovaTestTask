12d098a845d0b6b7a6347cd415e70beb4e54e42d
Zagr.Konst <zagr.konst@gmail.com>
2022-10-31 19:36:52 +0200

    initial commit

---
+++ src/main/java/com/zagr/konst/VeselovaTestTask/VeselovaTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zagr.konst.VeselovaTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeselovaTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeselovaTestTaskApplication.class, args);
+	}
+
+}
